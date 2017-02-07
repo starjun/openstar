85a179bfca88ee2b967df057d13e415405e4592e
starjun <18801180400@qq.com>
2017-02-07 11:34:55 +0800

    log阶段增加一个计数，后面有空在把host-uri计数也加上

--- log.lua
+++ log.lua
@@ -3,3 +3,8 @@ local optl = require("optl")
 local request_guid = ngx.ctx.request_guid
 optl.del_token(request_guid)
 
+-- 全局访问计数
+local gl_request_count = "global request count"
+optl.set_count_dict(gl_request_count)
+
+-- host - uri 计数
