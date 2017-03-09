aded923725f7ef83c3b98eff27e2555a23933ad0
starjun <18801180400@qq.com>
2017-03-09 17:22:57 +0800

    更新预留sync从redis同步相关配置相关代码....

--- i_worker.lua
+++ i_worker.lua
@@ -2,7 +2,6 @@
 
 if ngx.worker.id() ~= 0 then return end
 
-local timeAt = 5
 local handler
 
 -- dict 清空过期内存
@@ -13,8 +12,33 @@ local function flush_expired_dict()
 	end
 end
 
+-- 拉取数据
+local function pull_redisConfig()
+	
+end
+
+-- 推送统计计数等
+local function push_count_dict()
+	
+end
+
+-- 保存到本机文件
+local function save_configFile()
+	
+end
+
 handler = function()
 	-- do something
+	local config_dict = ngx.shared.config_dict
+	local config_base = cjson_safe.decode(config_dict:get("base")) or {}
+	local timeAt = config_base.autoSync.timeAt or 5
+
+	-- 如果 auto Sync 开启 就定时从redis 拉取配置并推送一些计数
+	if config_base.autoSync.state == "on" then
+
+	end
+
+	--清空过期内存
 	ngx.thread.spawn(flush_expired_dict)
 
 	--
